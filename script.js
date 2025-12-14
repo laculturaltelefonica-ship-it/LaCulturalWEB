3b0233994736d317364f6425d25c93d9193aa5f1
laculturaltelefonica-ship-it <laculturaltelefonica@gmail.com>
2025-12-14 23:25:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Web de Peña La Cultural cargada correctamente.");
+// Scroll suave con botón
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Navbar cambia al hacer scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
